35c44801226dfbc495df46e85b244d0f5df018b7
alicepradomelo <45275098+alicepradomelo@users.noreply.github.com>
2025-02-12 22:50:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ document.getElementById('nao').addEventListener('mouseover', function() {
     const newY = Math.random() * (containerRect.height - buttonRect.height);
 
     button.style.position = 'absolute';
-    button.style.left = ${newX}px;
-    button.style.top = ${newY}px;
+    button.style.left = `${newX}px`;
+    button.style.top = `${newY}px`;
 });
 
 // Botão "Sim" abre a planilha de filmes
 document.getElementById('sim').addEventListener('click', function() {
-
-    window.open('https://docs.google.com/spreadsheets/d/1-7mNwtbKc1akStiG8iJ5kEt2ow5Au7pe3drdwxmO2bM/edit'); // Substitua pelo link da sua planilha
+    window.open('https://docs.google.com/spreadsheets/d/SUA_PLANILHA_AQUI/edit?usp=sharing', '_blank'); // Substitua pelo link da sua planilha
 });
