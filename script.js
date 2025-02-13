bdaccf575afd4dbfda9aad7e5bb284279415db86
alicepradomelo <45275098+alicepradomelo@users.noreply.github.com>
2025-02-12 23:01:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,5 @@ document.getElementById('nao').addEventListener('mouseover', function() {
 });
 // Botão "Sim" abre a planilha de filmes
 document.getElementById('sim').addEventListener('click', function() {
-    window.open('https://docs.google.com/spreadsheets/d/1-7mNwtbKc1akStiG8iJ5kEt2ow5Au7pe3drdwxmO2bM/edit', '_blank'); // Substitua pelo link da sua planilha
+    window.open('https://docs.google.com/spreadsheets/d/1nxDP6m9KsTZEjoH25H1HQvL34KfBm6zOkF_XP8JeGCk/edit', '_blank'); // Substitua pelo link da sua planilha
 });
